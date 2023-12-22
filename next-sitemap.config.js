c76a634ee54098192289190f2194eba916707240
Suraj Auwal <siradjiawoual@gmail.com>
2023-12-22 20:00:26 +0100

    fix: changed priority for sitemaps

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,7 +2,8 @@
 const config = {
     siteUrl: 'https://trynanaapp.com',
     generateRobotsTxt: true,
-    generateIndexSitemap: false
+    generateIndexSitemap: false,
+    priority: 0.3
     // Other available options..
 }
 
