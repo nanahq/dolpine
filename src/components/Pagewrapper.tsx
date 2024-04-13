1d227622ca7da7bd471f6c23b59d05b428eef9ef
siradji <siradjiawoual@gmail.com>
2024-04-13 10:38:27 +0100

    feature: major refactor + overall site improvment

--- src/components/Pagewrapper.tsx
+++ src/components/Pagewrapper.tsx
@@ -1,10 +1,24 @@
-import React, { FC, PropsWithChildren, useMemo, useEffect } from 'react';
+import React, { FC, PropsWithChildren, useMemo } from 'react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { Header } from '@/components/layout/Header';
 import { Footer } from '@/components/layout/Footer';
-import Analytics from '@/components/analytics';
+import Script from 'next/script'
 
+import * as snippet from '@segment/snippet'
+
+function renderSnippet() {
+  const opts = {
+    apiKey: process.env.NEXT_PUBLIC_ANALYTICS_WRITE_KEY,
+    page: true,
+  }
+
+  if (process.env.NODE_ENV === 'development') {
+    return snippet.max(opts)
+  }
+
+  return snippet.min(opts)
+}
 
 interface Page {
   seo?: {
@@ -41,7 +55,10 @@ export const PageWrapper: FC<PropsWithChildren<PagewrapperProps>> = ({
       <Head>
         <title>{`${title} - Food Delivery`}</title>
         <meta name="description" content={description} />
-        <meta property="og:site_name" content={`${title}`} />
+        <meta
+          property="og:site_name"
+          content={`${title}`}
+        />
         <meta property="og:type" content="website" />
         <meta property="twitter:card" content="summary_large_image" />
         <meta
@@ -50,11 +67,17 @@ export const PageWrapper: FC<PropsWithChildren<PagewrapperProps>> = ({
           content={`${domain}${routePath}`}
         />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="og:title" content={`${title} - Food Delivery`} />
+        <meta
+          property="og:title"
+          content={`${title} - Food Delivery`}
+        />
         <meta property="og:description" content={description} />
         <meta property="og:image" content={`${domain}${image}`} />
         <meta property="twitter:url" content={`${domain}${routePath}`} />
-        <meta property="twitter:title" content={`${title} - Food Delivery`} />
+        <meta
+          property="twitter:title"
+          content={`${title} - Food Delivery`}
+        />
         <meta property="twitter:description" content={description} />
         <meta property="twitter:image" content={`${domain}${image}`} />
         <link
@@ -69,13 +92,16 @@ export const PageWrapper: FC<PropsWithChildren<PagewrapperProps>> = ({
           sizes="16x16"
           href="/favicon-16x16.png"
         />
+        <Script
+            id="segment-script"
+            dangerouslySetInnerHTML={{ __html: renderSnippet() }}
+        />
       </Head>
       <div className="md:container lg:py-5 pb-20">
         <Header />
         <main>{children}</main>
       </div>
       <Footer />
-      <Analytics />
     </div>
   );
 };
